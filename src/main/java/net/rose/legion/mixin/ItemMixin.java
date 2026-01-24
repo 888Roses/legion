0f28438d3aa8e084fe1d1897007c728a902e81da
Rosenoire <skillwarzaking@gmail.com>
2026-01-24 19:20:45 +0100

    Fixed potion effects still appearing in GUI

--- src/main/java/net/rose/legion/mixin/ItemMixin.java
+++ src/main/java/net/rose/legion/mixin/ItemMixin.java
@@ -5,27 +5,22 @@ import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ConsumableComponent;
 import net.minecraft.component.type.EquippableComponent;
 import net.minecraft.component.type.SuspiciousStewEffectsComponent;
-import net.minecraft.component.type.TooltipDisplayComponent;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.*;
 import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.item.consume.ConsumeEffect;
 import net.minecraft.item.tooltip.TooltipData;
-import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.text.Text;
 import net.rose.legion.common.tooltip.ArmorTooltipData;
 import net.rose.legion.common.tooltip.PotionTooltipData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
 
 @Mixin(Item.class)
 public class ItemMixin {
@@ -56,10 +51,10 @@ public class ItemMixin {
                 List<Float> chances = new ArrayList<>();
 
                 for (ConsumeEffect consumeEffect : consumeEffects) {
-                    if (consumeEffect instanceof ApplyEffectsConsumeEffect applyEffectsConsumeEffect) {
-                        applyEffectsConsumeEffect.effects().forEach(x -> {
+                    if (consumeEffect instanceof ApplyEffectsConsumeEffect(List<StatusEffectInstance> effects, float probability)) {
+                        effects.forEach(x -> {
                             statusEffectInstances.add(x);
-                            chances.add(applyEffectsConsumeEffect.probability());
+                            chances.add(probability);
                         });
                     }
                 }
@@ -82,13 +77,6 @@ public class ItemMixin {
         EquippableComponent equippableComponent = itemStack.get(DataComponentTypes.EQUIPPABLE);
         if (equippableComponent != null) {
             cir.setReturnValue(Optional.of(new ArmorTooltipData(itemStack)));
-        }
-    }
-
-    @Inject(method = "appendTooltip", at = @At("HEAD"), cancellable = true)
-    private void legion$appendTooltip(ItemStack stack, Item.TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> textConsumer, TooltipType type, CallbackInfo ci) {
-        if (stack.get(DataComponentTypes.POTION_CONTENTS) != null || stack.get(DataComponentTypes.SUSPICIOUS_STEW_EFFECTS) != null) {
-            ci.cancel();
         }
     }
 }
